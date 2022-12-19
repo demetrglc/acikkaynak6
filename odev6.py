2bfff327174aabc685698515bacc61be8a648058
Sennur Aldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-12-19 20:46:10 +0300

    Update odev6.py

--- odev6.py
+++ odev6.py
@@ -7,7 +7,7 @@ else:
    print("Adının harflerini listelendi")
     
     
-    
+ #(zehra şahaplıoğlu tarafından ekleme yapıldı.)   
 print("VÜCUT KİTLE ENDEKSİ HESAPLAMA PROGRAMI 💪")
 boy = float(input("Boy (m):"))
 kilo = int(input("Kilo (kg):"))
@@ -24,3 +24,26 @@ elif endeks > 30:
     print("\n ciddi obez VKİ:{}".format(endeks))
     
     
+ 
+#sayı tahmin oyunu(sennur aldemir tarafından ekleme yapıldı.)
+from random import randint
+ 
+rand=randint(1, 100)
+sayac=0
+ 
+while True:
+    sayac+=1
+    sayi=int(input("1 ile 100 arasında değer girin (0 çıkış):"))
+    if(sayi==0):
+        print("Oyunu İptal Ettiniz")
+        break
+    elif sayi < rand:
+        print("Daha Yüksek Bir Sayı Girin.")
+        continue
+    elif sayi > rand:
+        print("Daha Düşük Bir Sayı Girin.")
+        continue
+    else:
+        print("Rastele seçilen sayı {0}!".format(rand))
+        print("Tahmin sayınız {0}".format(sayac))
+﻿
